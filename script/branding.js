3ea396a3efeff529071410da1c5f13dd1b2cb13e
junggeuncocopam <72546781+junggeuncocopam@users.noreply.github.com>
2021-06-19 19:36:23 +0900

    Add files via upload

---
+++ script/branding.js
@@ -0,0 +1,39 @@
+$(function(){
+  const gstarImage = document.querySelectorAll('.gstarImage');
+  const graphicText = document.querySelectorAll('.graphic_text');
+  let currentItem = graphicText[0];
+
+  for (let i = 0; i < gstarImage.length; i++) {
+      gstarImage[i].dataset.index = i;
+      graphicText[i].dataset.index = i;
+  }
+  
+  function activate() {
+    currentItem.classList.add('visible');
+  }
+
+  function inactivate() {
+    currentItem.classList.remove('invisible');
+  }
+
+  window.addEventListener('scroll', () => {
+      let step;
+      let boundingRect;
+
+      for (let i = 0; i < gstarImage.length; i++) {
+          step = gstarImage[i];
+          boundingRect = step.getBoundingClientRect();
+          // console.log(boundingRect);
+          
+        //   utcItem = gstarImage[step.dataset.index]
+          if (boundingRect.top > window.innerHeight * 0 &&
+              boundingRect.top < window.innerHeight * 0.9) {
+                inactivate();
+                currentItem = graphicText[step.dataset.index];
+                activate();
+              }
+              activate();
+      }
+      
+  });
+});
---
+++ script/main.js
@@ -0,0 +1,107 @@
+// main.js
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+	
+	//Navigation
+
+	var app = function () {
+		var body = undefined;
+		var menu = undefined;
+		var menuItems = undefined;
+		var init = function init() {
+			body = document.querySelector('body');
+			menu = document.querySelector('.menu-icon');
+			menuItems = document.querySelectorAll('.nav__list-item');
+			applyListeners();
+		};
+		var applyListeners = function applyListeners() {
+			menu.addEventListener('click', function () {
+				return toggleClass(body, 'nav-active');
+			});
+		};
+		var toggleClass = function toggleClass(element, stringClass) {
+			if (element.classList.contains(stringClass)) element.classList.remove(stringClass);else element.classList.add(stringClass);
+		};
+		init();
+	}();
+
+})(jQuery); 
+
+// background motion
+const elems = document.querySelectorAll('.laya-please');
+const layer2 = document.querySelector('.layer-2');
+const layer3 = document.querySelector('.layer-3');
+const layer4 = document.querySelector('.layer-4');
+const layer5 = document.querySelector('.layer-5');
+
+
+setTimeout(function () {
+    elems.forEach(function (elem, index) {
+        elem.style.animation = "none";
+    });
+}, 1500);
+
+
+
+document.body.addEventListener('mousemove', function (e) {
+    if (!e.currentTarget.dataset.triggered) {
+        elems.forEach(function (elem, index) {
+            if (elem.getAttribute('style')) {
+                elem.style.transition = "all .5s";
+                elem.style.transform = "none";
+            }
+        });
+    }
+    e.currentTarget.dataset.triggered = true;
+    
+    let width = window.innerWidth / 2;
+    let mouseMoved2 = ((width - e.pageX) / 50);
+    let mouseMoved3 = ((width - e.pageX) / 40);
+    let mouseMoved4 = ((width - e.pageX) / 60);
+
+    layer3.style.transform = "translateX(" + mouseMoved2 + "px)";
+    layer4.style.transform = "translateX(" + mouseMoved3 + "px)";
+    layer5.style.transform = "translateX(" + mouseMoved4 + "px)";
+
+});
+
+document.body.addEventListener('mouseleave', function (e) {
+    elems.forEach(function (elem, index) {
+        elem.style.transition = "all .5s";
+        elem.style.transform = "none";
+    });
+});
+
+document.body.addEventListener('mouseenter', function (e) {
+    elems.forEach(function (elem, index) {
+        setTimeout(function () {
+            elem.style.transition = "none";
+        }, 500);
+    });
+});
